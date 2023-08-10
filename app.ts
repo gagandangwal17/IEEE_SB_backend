8614019f792cd0c07f2124f8da52e94b555260fd
Ayam Dobhal <me@iamdobhal.dev>
2023-08-11 01:25:06 +0530

    fix contact us

--- app.ts
+++ app.ts
@@ -1,17 +1,15 @@
-import express from 'express'
-import { connect } from './db-connector';
-import cors from 'cors';
-import contactus from './controllers/contact-us.controller';
-import register from './controllers/register.controller';
-import router from './routes/main';
+import cors from "cors";
+import express from "express";
+import { connect } from "./db-connector";
+import router from "./routes/main";
 
 const app = express();
 
 (async () => {
-    await connect()
-    app.use(express.json())
-    app.use(cors())
-    app.use('/api', router);
-  app.listen(2000)
-  console.log('Server started on port 3000')
-})()
+  await connect();
+  app.use(express.json());
+  app.use(cors());
+  app.use("/api", router);
+  app.listen(2000);
+  console.log("Server started on port 3000");
+})();
