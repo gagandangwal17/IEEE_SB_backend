c6f2063f6b8d165de82cb46dcfec128cfe32ee8c
Ayam Dobhal <me@iamdobhal.dev>
2023-08-11 01:15:53 +0530

    edit port

--- app.ts
+++ app.ts
@@ -12,6 +12,6 @@ const app = express();
     app.use(express.json())
     app.use(cors())
     app.use('/api', router);
-  app.listen(3000)
+  app.listen(2000)
   console.log('Server started on port 3000')
 })()
